518cacc0c0d0c407df7546f62e43e1379e05751d
FrankHWD <hewendi1999@163.com>
2022-03-26 01:24:46 +0800

    Add tests of library-EMA.

--- library-udf/src/test/java/org/apache/iotdb/library/smoothing/SmoothingTests.java
+++ library-udf/src/test/java/org/apache/iotdb/library/smoothing/SmoothingTests.java
@@ -162,6 +162,10 @@ public class SmoothingTests {
       statement.execute("create function dema as 'org.apache.iotdb.library.smoothing.UDTFDEMA'");
       statement.execute(
           "create function TEMA as 'org.apache.iotdb.library.smoothing.UDTFTEMA'");
+      statement.execute(
+              "create function RSI as 'org.apache.iotdb.library.smoothing.UDTFRSI'");
+      statement.execute(
+              "create function TRIX as 'org.apache.iotdb.library.smoothing.UDTFTRIX'");
     } catch (SQLException throwable) {
       fail(throwable.getMessage());
     }
@@ -218,6 +222,38 @@ public class SmoothingTests {
       while (resultSet.next()) {
         double result1 = resultSet.getDouble(1);
         Assert.assertTrue(Math.abs(result1)<3.1);
+      }
+    } catch (SQLException throwable) {
+      fail(throwable.getMessage());
+    }
+  }
+
+  @Test
+  public void testTRIX1() {
+    String sqlStr = "select trix(d1.s1,'window'='12') from root.season";
+    try (Connection connection = EnvFactory.getEnv().getConnection();
+         Statement statement = connection.createStatement()) {
+      ResultSet resultSet = statement.executeQuery(sqlStr);
+      resultSet.next();
+      while (resultSet.next()) {
+        double result1 = resultSet.getDouble(1);
+        Assert.assertTrue(Math.abs(result1)<5);
+      }
+    } catch (SQLException throwable) {
+      fail(throwable.getMessage());
+    }
+  }
+
+  @Test
+  public void testRSI1() {
+    String sqlStr = "select rsi(d1.s1,'window'='12') from root.season";
+    try (Connection connection = EnvFactory.getEnv().getConnection();
+         Statement statement = connection.createStatement()) {
+      ResultSet resultSet = statement.executeQuery(sqlStr);
+      resultSet.next();
+      while (resultSet.next()) {
+        double result1 = resultSet.getDouble(1);
+        Assert.assertTrue(Math.abs(result1)<5);
       }
     } catch (SQLException throwable) {
       fail(throwable.getMessage());

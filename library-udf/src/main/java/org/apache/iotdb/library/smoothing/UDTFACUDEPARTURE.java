74fe44c2530f38b23db45ca851bf184f187f06f1
FrankHWD <hewendi1999@163.com>
2022-05-10 16:38:40 +0800

    Create functions of acumulate departure.

--- library-udf/src/main/java/org/apache/iotdb/library/smoothing/UDTFACUDEPARTURE.java
+++ library-udf/src/main/java/org/apache/iotdb/library/smoothing/UDTFACUDEPARTURE.java
@@ -21,6 +21,7 @@ package org.apache.iotdb.library.smoothing;
 
 import org.apache.iotdb.db.query.udf.api.UDTF;
 import org.apache.iotdb.db.query.udf.api.access.Row;
+import org.apache.iotdb.db.query.udf.api.access.RowWindow;
 import org.apache.iotdb.db.query.udf.api.collector.PointCollector;
 import org.apache.iotdb.db.query.udf.api.customizer.config.UDTFConfigurations;
 import org.apache.iotdb.db.query.udf.api.customizer.parameter.UDFParameterValidator;
@@ -29,20 +30,24 @@ import org.apache.iotdb.db.query.udf.api.customizer.strategy.RowByRowAccessStrat
 import org.apache.iotdb.library.util.Util;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
 
-/** This function calculates the euclidean distance between two input series. */
-public class UDAFEuclidDis implements UDTF {
+/** Acumulate Departure */
+public class UDTFACUDEPARTURE implements UDTF {
 
-  private long count = 0;
-  private double sum = 0.0;
+  private double sum = 0;
+  private double num = 0;
+  private double avg = 0;
+  private double all_avg = 0;
+  private double all_sum = 0;
+  private double all_num = 0;
+  private int n = 0;
+  private TSDataType dataType;
 
   @Override
   public void validate(UDFParameterValidator validator) throws Exception {
     validator
-        .validateInputSeriesNumber(2)
+        .validateInputSeriesNumber(1)
         .validateInputSeriesDataType(
-            0, TSDataType.INT32, TSDataType.INT64, TSDataType.FLOAT, TSDataType.DOUBLE)
-        .validateInputSeriesDataType(
-            1, TSDataType.INT32, TSDataType.INT64, TSDataType.FLOAT, TSDataType.DOUBLE);
+            0, TSDataType.INT32, TSDataType.INT64, TSDataType.FLOAT, TSDataType.DOUBLE);
   }
 
   @Override
@@ -51,29 +56,37 @@ public class UDAFEuclidDis implements UDTF {
     configurations
         .setAccessStrategy(new RowByRowAccessStrategy())
         .setOutputDataType(TSDataType.DOUBLE);
-    count = 0;
-    sum = 0.0;
+    sum = 0;
+    all_sum = 0;
+    num = 0;
+    all_num = 0;
+    avg = 0;
+    all_avg = 0;
+    n = 0;
+    dataType = parameters.getDataType(0);
   }
 
   @Override
-  public void transform(Row row, PointCollector collector) throws Exception {
-    if (row.isNull(0) || row.isNull(1)) {
-      return;
+  public void transform(RowWindow rowWindow, PointCollector collector) throws Exception {
+    n = rowWindow.windowSize();
+    for (int i = 0; i < n; i++) {
+      Row row = rowWindow.getRow(i);
+      all_sum += row.getDouble(i);
+      all_num += 1;
     }
-    double x = Util.getValueAsDouble(row, 0);
-    double y = Util.getValueAsDouble(row, 1);
-    if (Double.isFinite(x) && Double.isFinite(y)) {
-      count++;
-      sum += (x - y) * (x - y);
+    all_avg = all_sum / all_num;
+    for (int i = 0; i < n; i++) {
+      Row row = rowWindow.getRow(i);
+      sum += row.getDouble(i);
+      num += 1;
+      avg = sum / num;
+      Util.putValue(collector, dataType, row.getTime(), avg - all_avg);
     }
   }
 
   @Override
   public void terminate(PointCollector collector) throws Exception {
-    if (count > 0) {
-      double distance = Math.sqrt(sum);
-      collector.putDouble(0, distance);
-    } else {
+    if (n == 0) {
       collector.putDouble(0, Double.NaN);
     }
   }
